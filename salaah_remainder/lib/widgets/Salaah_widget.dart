c425161bcec090d553dfa6e4c3268dc7d2ba69ba
Fareed <90202062+Fareed-Ahmad7@users.noreply.github.com>
2022-03-01 00:33:05 +0530

    salaah_timings and notify

--- salaah_remainder/lib/widgets/Salaah_widget.dart
+++ salaah_remainder/lib/widgets/Salaah_widget.dart
@@ -1,5 +1,6 @@
 // .  As-Salaah list
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Salaah extends StatelessWidget {
@@ -8,22 +9,62 @@ class Salaah extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView(
-      children: const [
-        ListTile(
-          title: Text('fajr'),
-        ),
-        ListTile(
-          title: Text('zuhr'),
-        ),
-        ListTile(
-          title: Text('asr'),
-        ),
-        ListTile(
-          title: Text('magrib'),
-        ),
-        ListTile(
-          title: Text('isha'),
-        )
+      children: [
+            ListTile(
+              title: const Text('fajr'),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: const [
+                  Text('05:26 AM'),
+                  SizedBox(width:30),
+                  Icon(CupertinoIcons.bell_circle)
+                ],
+              )
+            ),
+            ListTile(
+              title: const Text('dhuhr'),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: const [
+                  Text('12:38 PM'),
+                  SizedBox(width:30),
+                  Icon(CupertinoIcons.bell_circle)
+                ],
+              )
+            ),
+            ListTile(
+              title: const Text('asr'),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: const [
+                  Text('03:55 PM'),
+                  SizedBox(width:30),
+                  Icon(CupertinoIcons.bell_circle)
+                ],
+              )
+            ),
+            ListTile(
+              title: const Text('magrib'),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: const [
+                  Text('06:31 PM'),
+                  SizedBox(width:30),
+                  Icon(CupertinoIcons.bell_circle)
+                ],
+              )
+            ),
+            ListTile(
+              title: const Text('isha'),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: const [
+                  Text('07:40 PM'),
+                  SizedBox(width:30),
+                  Icon(CupertinoIcons.bell_circle)
+                ],
+              )
+            ),
       ],
     );
   }
